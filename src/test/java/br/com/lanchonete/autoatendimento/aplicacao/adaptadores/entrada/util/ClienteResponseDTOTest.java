277f42a6e06c2a8dabb4a4ec7ff45807f6377d45
Anderson <anderson.fer@camara.leg.br>
2025-04-15 12:35:57 -0300

    mudança no nome da classe

--- src/test/java/br/com/lanchonete/autoatendimento/aplicacao/adaptadores/entrada/util/ClienteResponseDTOTest.java
+++ src/test/java/br/com/lanchonete/autoatendimento/aplicacao/adaptadores/entrada/util/ClienteResponseDTOTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClienteMapperTest {
+class ClienteResponseDTOTest {
 
     @Test
     @DisplayName("Deve converter Cliente para ClienteResponseDTO corretamente")
